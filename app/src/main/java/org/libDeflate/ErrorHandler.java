a6b07f4146c1542f7a866cefc52e74ed44d40a0f
n9tank <n9tank@163.com>
2025-01-19 08:37:02 +0800

    尝试优化

--- app/src/main/java/org/libDeflate/ErrorHandler.java
+++ app/src/main/java/org/libDeflate/ErrorHandler.java
@@ -4,31 +4,52 @@ import java.util.Vector;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicReferenceArray;
 import java.util.concurrent.atomic.LongAdder;
 
-public abstract class ErrorHandler {
+public class ErrorHandler {
+ public AtomicReferenceArray<Future> cache;
  public volatile Vector<Future> flist=new Vector();
  public ExecutorService pool;
  public LongAdder io=new LongAdder();
- public boolean igron;
  public Vector<Throwable> err=new Vector();
+ public volatile UIPost ui;
  public Canceler can;
  public ErrorHandler(ExecutorService pool, Canceler can) {
   this.pool = pool;
   this.can = can;
  }
+ public void setCache() {
+  this.cache = new AtomicReferenceArray(Runtime.getRuntime().availableProcessors() << 1);
+ }
  public void add(Callable call) throws IOException {
-  Vector<Future> flist=this.flist;
-  if (flist == null)throw new IOException();
+  if (iscancel())throw new IOException();
   io.increment();
-  flist.add(pool.submit(call));
+  fadd(call, false);
+ }
+ public void fadd(Callable call, boolean isCache) {
+  Future fu = pool.submit(call);
+  //尝试取活跃的任务避免数组扩张
+  if (isCache) {
+   AtomicReferenceArray<Future> cache=this.cache;
+   if (cache != null) {
+    for (int i=0,len=cache.length();i < len;++i) {
+     Future check=cache.get(i);
+     if (check == null || check.isDone()) {
+      if (!cache.compareAndSet(i, check, fu))
+       continue;
+      return;
+     }
+    }
+   }
+  }
+  flist.add(fu);
  }
  public void addN(Callable call) {
-  Vector<Future> flist=this.flist;
-  if (flist == null)pop();
-  else flist.add(pool.submit(call));
+  if (iscancel())pop();
+  else fadd(call, true);
  }
- public boolean iscancel() {
+ public final boolean iscancel() {
   return flist == null;
  }
  public void pop() {
@@ -42,7 +63,7 @@ public abstract class ErrorHandler {
  public void onError(Throwable e) {
   if (e instanceof InterruptedException)return;
   err.add(e);
-  if (!igron)can.cancel();
+  can.cancel();
  }
  public boolean cancel() {
   Vector<Future> list=flist;
@@ -52,6 +73,16 @@ public abstract class ErrorHandler {
    if (list != null)this.flist = null;
    else return false;
   }
+  AtomicReferenceArray<Future> cache=this.cache;
+  if (cache != null) {
+   for (int i=0,len=cache.length();i < len;++i) {
+    Future fu= cache.get(i);
+    if (fu == null)break;
+    boolean isrun=fu.cancel(true);
+    if (!isrun && fu.isCancelled())
+     pop();
+   }
+  }
   for (Future fu:list) {
    boolean isrun=fu.cancel(true);
    if (!isrun && fu.isCancelled())
@@ -59,5 +90,9 @@ public abstract class ErrorHandler {
   }
   return true;
  }
- public abstract void onClose();
+ public void onClose() {
+  UIPost ui=this.ui;
+  if (ui != null)
+   ui.accept(err);
+ }
 }
